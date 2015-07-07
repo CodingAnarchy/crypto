273ae2766a61088d8d2cd3bd79ac20ae82dc30a2
CodingAnarchy <mtanous22@gmail.com>
2015-07-06 23:55:07 -0600

    Fix test case

--- test/classical_test.rs
+++ test/classical_test.rs
@@ -2,7 +2,6 @@ use super::super::lib;
 
 #[test]
 fn test_atbash_encrypt() {
-    let mut s = String::new();
-    s.push_str("Defend the east wall of the castle.");
+    let s = "Defend the east wall of the castle.";
     assert_eq!("wvuvmwgsvvzhgdzoolugsvxzhgov", lib::classical::atbash_encrypt(s).as_str())
 }
