f2155ddea201a6fefc71839d6cfb4fde959d6bfa
CodingAnarchy <mtanous22@gmail.com>
2015-07-06 19:43:29 -0600

    Updates to travis file and crypto.rs main file

--- crypto.rs
+++ crypto.rs
@@ -1,15 +1,26 @@
+#![feature(concat_idents)]
+
 mod lib;
 use std::env;
+use std::collections::HashMap;
+use syntax::parse::token;
 
 #[cfg(test)]
 mod test;
 
+#[cfg_attr(test, allow(dead_code))]
 fn main() {
-    let mut argv: Vec<_> = env::args().collect();
+    let argv: Vec<_> = env::args().collect();
     if argv.len() < 3 {
         println!("Error: expected two arguments - cipher and message.");
         return
     }
 
     let ciphers = ["atbash", "affine"];
+
+    if ciphers.contains(argv[1]) {
+        let f = concat_idents!(token::str_to_ident(argv[1]), _encrypt);
+        let e = f("hello");
+        println!("{}", e);
+    }
 }
