9c931223ed1cbb50b41f6bcb1c5dcd599ffe3740
CodingAnarchy <mtanous22@gmail.com>
2015-07-06 23:53:29 -0600

    Add decryption function and fix offset correction

--- lib/classical.rs
+++ lib/classical.rs
@@ -9,9 +9,20 @@ pub fn affine_encrypt(a: u32, b: u32, msg: &str) -> String {
     let mut e = String::new();
     for ch in msg.chars() {
         if !ch.is_alphabetic() { continue; }
-        let p = ch.to_digit(36).unwrap() - 11;
-        let c = char::from_digit((a * p + b) % 26 + 9, 36).unwrap();
+        let p = ch.to_digit(36).unwrap() - 10;  // Need to shift down by 11 for range of 0-25
+        let c = char::from_digit((a * p + b) % 26 + 10, 36).unwrap();
         e.push(c);
     }
     return e;
+}
+
+pub fn affine_decrypt(a: u32, b: u32, e: &str) -> String {
+    let mut msg = String::new();
+    for ch in e.chars() {
+        if !ch.is_alphabetic() { continue; }
+        let c = ch.to_digit(36).unwrap() - 10;  // Need to shift down by 11 for range of 0-25
+        let p = char::from_digit(((c- b) / a) % 26 + 10, 36).unwrap();
+        msg.push(p);
+    }
+    return msg;
 }
--- test/classical_test.rs
+++ test/classical_test.rs
@@ -4,5 +4,5 @@ use super::super::lib;
 fn test_atbash_encrypt() {
     let mut s = String::new();
     s.push_str("Defend the east wall of the castle.");
-    assert_eq!("wvuvmwgsvvzhgdzoolugsvxzhgov", lib::classical::atbash_encrypt(s))
+    assert_eq!("wvuvmwgsvvzhgdzoolugsvxzhgov", lib::classical::atbash_encrypt(s).as_str())
 }
