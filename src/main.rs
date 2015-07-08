a7603b5ee885e1bc893b6c512f2f7e32f4222453
CodingAnarchy <mtanous22@gmail.com>
2015-07-07 18:31:33 -0600

    Add code to allow user input of needed parameters for affine cipher

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate num;
 
 mod lib;
 use std::env;
+use std::io;
+use std::io::prelude::*;
+use num::integer::gcd;
 
 #[cfg_attr(test, allow(dead_code))]
 fn main() {
@@ -13,9 +16,32 @@ fn main() {
     }
 
     let msg = argv[2].as_str();
+    let mut stdin = io::stdin();
+    let mut input = String::new();
 
     match argv[1].as_str() {
-        "atbash" => { let e = lib::classical::atbash_encrypt(msg); println!("Ciphertext: {}", e.as_str()); },
+        "atbash" => {
+            let e = lib::classical::atbash_encrypt(msg);
+            println!("Ciphertext: {}", e.as_str());
+        },
+        "affine" => {
+            let mut a: u32 = 26;
+            let mut b: u32 = 26;
+            while gcd(a, 26) != 1 || a > 25 {
+                println!("Enter a (must be relatively prime with 26): ");
+                stdin.read_line(&mut input).unwrap();
+                a = input.trim_right().parse::<u32>().ok().unwrap();
+                input.clear();
+            }
+            while b > 25 {
+                println!("Enter b (must be between 1 and 25): ");
+                stdin.read_line(&mut input).unwrap();
+                b = input.trim_right().parse::<u32>().ok().unwrap();
+                input.clear();
+            }
+            let e = lib::classical::affine_encrypt(a, b, msg);
+            println!("Ciphertext: {}", e.as_str());
+        },
         _ => println!("Invalid cipher selected: {}", argv[1])
     }
 }
