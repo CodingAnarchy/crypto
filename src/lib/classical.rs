92df8fe917ee5de22ae653d8870adf81a26d81c2
CodingAnarchy <mtanous22@gmail.com>
2015-07-07 19:00:22 -0600

    Rewrite affine_decrypt function to fix bugs discovered by new tests

--- src/lib/classical.rs
+++ src/lib/classical.rs
@@ -1,6 +1,4 @@
 use std::char;
-use num::integer::gcd;
-use num::abs;
 
 pub fn atbash_encrypt(msg: &str) -> String {
     return affine_encrypt(25, 25, msg);
@@ -11,7 +9,7 @@ pub fn affine_encrypt(a: u32, b: u32, msg: &str) -> String {
     let mut e = String::new();
     for ch in msg.chars() {
         if !ch.is_alphabetic() { continue; }
-        let p = ch.to_digit(36).unwrap() - 10;  // Need to shift down by 11 for range of 0-25
+        let p = ch.to_digit(36).unwrap() - 10;  // Need to shift down by 10 for range of 0-25
         let c = char::from_digit((a * p + b) % 26 + 10, 36).unwrap();
         e.push(c);
     }
@@ -23,13 +21,17 @@ pub fn atbash_decrypt(msg: &str) -> String {
 }
 
 pub fn affine_decrypt(a: u32, b: u32, e: &str) -> String {
-    let x = gcd(a, 26) % 26;
+    let mut x = 0;
+    for n in 1..26 {
+        if (a * n) % 26 == 1 { x = n; break; }
+    }
+    println!("X: {}", x);
     let mut msg = String::new();
     for ch in e.chars() {
         if !ch.is_alphabetic() { continue; }
-        let c = ch.to_digit(36).unwrap() - 10;  // Need to shift down by 11 for range of 0-25
-        let sub = c as i32 - b as i32;
-        let p = char::from_digit((abs(sub) as u32 * x) % 26 + 10, 36).unwrap();
+        let c = ch.to_digit(36).unwrap() - 10;  // Need to shift down by 10 for range of 0-25
+        let sub = ((26 + (c as i32 - b as i32)) % 26) as u32;
+        let p = char::from_digit((sub * x) % 26 + 10, 36).unwrap();
         msg.push(p);
     }
     return msg;
