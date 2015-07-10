20120d4869e270440f3d90a2dfb859471603bc5c
CodingAnarchy <mtanous22@gmail.com>
2015-07-09 18:16:37 -0600

    Add affine decrypt to options

--- src/main.rs
+++ src/main.rs
@@ -22,34 +22,52 @@ fn main() {
 
     match argv[1].as_str() {
         "encrypt" => match argv[2].as_str() {
-                "atbash" => {
-                    let e = lib::classical::atbash_encrypt(msg);
-                    println!("Ciphertext: {}", e.as_str());
-                },
-                "affine" => {
-                    let mut a: u32 = 26;
-                    let mut b: u32 = 26;
-                    while gcd(a, 26) != 1 || a > 25 {
-                        println!("Enter a (must be relatively prime with 26): ");
-                        stdin.read_line(&mut input).unwrap();
-                        a = input.trim_right().parse::<u32>().ok().unwrap();
-                        input.clear();
-                    }
-                    while b > 25 {
-                        println!("Enter b (must be between 1 and 25): ");
-                        stdin.read_line(&mut input).unwrap();
-                        b = input.trim_right().parse::<u32>().ok().unwrap();
-                        input.clear();
-                    }
-                    let e = lib::classical::affine_encrypt(a, b, msg);
-                    println!("Ciphertext: {}", e.as_str());
-                },
-                _ => println!("Invalid cipher selected: {}", argv[2])
+            "atbash" => {
+                let e = lib::classical::atbash_encrypt(msg);
+                println!("Ciphertext: {}", e.as_str());
             },
+            "affine" => {
+                let mut a: u32 = 26;
+                let mut b: u32 = 26;
+                while gcd(a, 26) != 1 || a > 25 {
+                    println!("Enter a (must be relatively prime with 26): ");
+                    stdin.read_line(&mut input).unwrap();
+                    a = input.trim_right().parse::<u32>().ok().unwrap();
+                    input.clear();
+                }
+                while b > 25 {
+                    println!("Enter b (must be between 1 and 25): ");
+                    stdin.read_line(&mut input).unwrap();
+                    b = input.trim_right().parse::<u32>().ok().unwrap();
+                    input.clear();
+                }
+                let e = lib::classical::affine_encrypt(a, b, msg);
+                println!("Ciphertext: {}", e.as_str());
+            },
+            _ => println!("Invalid cipher selected: {}", argv[2])
+        },
         "decrypt" => match argv[2].as_str() {
             "atbash" => {
                 let s = lib::classical::atbash_decrypt(msg);
                 println!("Plain text: {}", s.as_str());
+            },
+            "affine" => {
+                let mut a: u32 = 26;
+                let mut b: u32 = 26;
+                while a > 25 {
+                    println!("Enter a (must be between 1 and 25): ");
+                    stdin.read_line(&mut input).unwrap();
+                    a = input.trim_right().parse::<u32>().ok().unwrap();
+                    input.clear();
+                }
+                while b > 25 {
+                    println!("Enter b (must be between 1 and 25): ");
+                    stdin.read_line(&mut input).unwrap();
+                    b = input.trim_right().parse::<u32>().ok().unwrap();
+                    input.clear();
+                }
+                let e = lib::classical::affine_decrypt(a, b, msg);
+                println!("Ciphertext: {}", e.as_str());
             },
             _ => println!("Invalid cipher selected: {}", argv[2])
         },
