c69048ac20ec20c465c2c807f3be10e1e0d8be74
CodingAnarchy <mtanous22@gmail.com>
2015-07-11 18:00:53 -0600

    Add utils.rs with cipher_alphabet function

---
+++ src/lib/utils.rs
@@ -0,0 +1,26 @@
+static ASCII_LOWER: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+pub fn cipher_alphabet(keyword: &str) -> String {
+    let mut alpha = String::new();
+    for ch in keyword.chars() {
+        if !ch.is_alphabetic() || alpha.contains(ch) { continue; }
+        alpha.push(ch);
+    }
+    for c in ASCII_LOWER.iter() {
+        if alpha.contains(*c) { continue; }
+        alpha.push(*c);
+    }
+    return alpha;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_alphabet() {
+        let k = "royalnewzealandnavy";
+        assert_eq!("royalnewzdvbcfghijkmpqstux", cipher_alphabet(k));
+    }
+}
